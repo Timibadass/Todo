e878d4d84e28a4bc67e883bb102a5380f416d276
Timibadass <oblacishere@gmail.com>
2018-10-23 20:48:00 +0100

    heroku port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 7000);
+app.listen(process.env.PORT || 8080);
 
 app.get('*', ( req, res ) => {
     res.sendFile( __dirname + '/dist/index.html');
