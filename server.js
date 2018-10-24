795d0d036a42ee2f02120c0055f8a57a845ecead
Timibadass <oblacishere@gmail.com>
2018-10-24 21:36:54 +0100

    added history middleware

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 //Install express server
+var history = require('connect-history-api-fallback');
 const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(history());
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-// Start the app by listening on the default Heroku port
 
 app.get('/.*/', ( req, res ) => {
   res.sendFile( __dirname + '/dist/index.html');
 });
 
-app.listen(port);
+// Start the app by listening on the default Heroku port
+app.listen(port, () => {
+  console.log('Server listening on: http://localhost:%s', port);
+});
