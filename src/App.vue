ed3beff2228d29cae55713e40ce460785e350e5d
Timibadass <oblacishere@gmail.com>
2019-02-16 17:32:31 +0100

    set up prerendering properly

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" data-server-rendered="true">
     <header-component></header-component>
     <router-view />
   </div>
--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,23 @@ const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
 const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
-module.exports = {
-    plugins: [
-      new PrerenderSPAPlugin({
+const prodPlugin = [
+    new PrerenderSPAPlugin({
         // Required - The path to the webpack-outputted app to prerender.
         staticDir: path.join(__dirname, 'dist'),
         // Required - Routes to render.
         routes: [ '/', '/todos', '/add', '/todos/completed' ],
         renderer: new Renderer({
             renderAfterElementExists: '#app',
-            headless: false // Display the browser window when rendering. Useful for debugging.
-          })
-      })
-    ]
+            // headless: false // Display the browser window when rendering. Useful for debugging.
+        })
+    })
+]
+
+module.exports = {
+    configureWebpack: config  => {
+        if (process.env.NODE_ENV === 'production') {
+            config.plugins.push(...prodPlugin) 
+        } else return;
+    }
   }
